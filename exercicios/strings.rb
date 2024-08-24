6e050874f7b1bd47ebca3cf880a5d4357cfc2cc6
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-08-24 15:40:54 -0300

    up

---
+++ exercicios/strings.rb
@@ -0,0 +1,5 @@
+puts("Bem-vindo ao sistema, Digite seu nome!");
+
+nome = gets.chomp();
+
+puts "Olá #{nome}, um prazer ter você conosco!";
--- strings.rb
+++
@@ -1,7 +0,0 @@
-#Adicionando comentario
-
-nome = "Ewerton";
-idade = 34;
-
-#Ao adicionar um numero junto com uma string, era retornar erro. Por isso deve-se converter o tipo number para string.
-puts("Olá, meu nome é: " + nome + " e tenho " + idade.to_s() + " anos");
